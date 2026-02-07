14a4d2521e28d9e49fbecf51cff2650d395461ab
adityakhilare04 <aditya.khilare04@gmail.com>
2026-02-08 01:24:53 +0530

    React component

--- index.js
+++ index.js
@@ -1,19 +1,15 @@
-const heading = React.createElement(
-    "div", { id: "parent" },
-    [
-        React.createElement("div", { id: "child1" },
-        [
-            React.createElement("h1", {}, "I am h1 tag" ),
-            React.createElement("h2", {}, "I am h2 tag" )
-        ]),
-        React.createElement("div", { id: "child2" },
-        [
-            React.createElement("h1", {}, "I am h1 tag" ),
-            React.createElement("h2", {}, "I am h2 tag" )
-        ])
-    ]
-);
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-const root = document.getElementById("root");
-const rootElement = ReactDOM.createRoot(root);
-rootElement.render(heading);
+const TitleComponent = () => (
+    <h1 className="heading">Namaste React Functional Component</h1>
+)
+const HeadingComponent = () => (
+    <div className="container">
+        <TitleComponent />
+        <h1>This is a functional component.</h1>
+    </div>
+)
+
+const rootElement = ReactDOM.createRoot(document.getElementById("root"));
+rootElement.render(<HeadingComponent />);
